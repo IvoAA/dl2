2651597222634f74ec10e4a083b7be5ca69f2372
Jonas Weile <s173906@student.dtu.dk>
2022-05-26 17:52:06 +0200

    Split data into train, val, and test

--- training/supervised/mimic3_treated.py
+++ training/supervised/mimic3_treated.py
@@ -1,6 +1,6 @@
+from tokenize import String
 import torch
 from torch.utils.data import Dataset
-import sys
 import os
 
 from typing import Any, Tuple
@@ -10,14 +10,10 @@ from sklearn.preprocessing import StandardScaler
 import numpy as np
 from torchvision.transforms import transforms
 
-PATH = 'Users/ivo00/Desktop/Ivo/DTU/Courses/Semester 2/Special Course'
-
-mimic3_dir = f"C:/{PATH}/mimic3-benchmarks-master"
-sys.path.append(mimic3_dir)
-
 from mimic3benchmark.readers import InHospitalMortalityReader
 from mimic3models import common_utils
 
+CACHED_DIR = os.path.join("data/cached")
 
 def read_and_extract_features(reader):
     ret = common_utils.read_chunk(reader, reader.get_number_of_examples())
@@ -27,24 +23,23 @@ def read_and_extract_features(reader):
     return X, ret['y'], ret['name']
 
 
-def get_cached_filename(input: bool, folder: str) -> str:
-    cached_dir = "datasets/cached"
+def get_cached_filename(input: bool, mode: str) -> str:
     if input:
-        filename = os.path.join(cached_dir, f"{folder}_X_cached.pt")
+        filename = os.path.join(CACHED_DIR, f"{mode}_X_cached.pt")
     else:
-        filename = os.path.join(cached_dir, f"{folder}_y_cached.pt")
+        filename = os.path.join(CACHED_DIR, f"{mode}_y_cached.pt")
     return filename
 
         
-def cached_data_exists(folder: str) -> bool:
-    X_cached = get_cached_filename(True, folder)
-    y_cached = get_cached_filename(False, folder)
+def cached_data_exists(mode: str) -> bool:
+    X_cached = get_cached_filename(True, mode)
+    y_cached = get_cached_filename(False, mode)
     return os.path.exists(X_cached) and os.path.exists(y_cached)
 
 
-def get_cached_data(folder: str):
-    X_cached = get_cached_filename(True, folder)
-    y_cached = get_cached_filename(False, folder)
+def get_cached_data(mode: str):
+    X_cached = get_cached_filename(True, mode)
+    y_cached = get_cached_filename(False, mode)
     X = torch.load(X_cached)
     y = torch.load(y_cached)
     return (X, y)
@@ -54,18 +49,23 @@ class MIMIC3(Dataset):
 
     def __init__(
             self,
-            train: bool = True,
+            mode: str,
             download: bool = True,
             use_cached: bool = True,
     ) -> None:
         self.transform = transforms.Compose([transforms.ToTensor()])
-        folder = 'train' if train else 'test'
+        
+        assert mode in ['train', 'val', 'test'], 'Dataset must be either train, val or test!'
+        if mode in ['train', 'val']:
+            folder = 'train'
+        else:
+            folder = 'test'    
 
         # Check if we should use cached data
-        if use_cached and cached_data_exists(folder):
-            X, y = get_cached_data(folder)
+        if use_cached and cached_data_exists(mode):
+            X, y = get_cached_data(mode)
         else:
-            X, y = self.prepare_data(folder, download)
+            X, y = self.prepare_data(mode, folder, download)
 
         # small fix - make sure the data always contains an even number of data points
         if len(X) % 2 > 0:
@@ -75,10 +75,9 @@ class MIMIC3(Dataset):
         self.data = (X, y)
 
 
-
-    def prepare_data(self, folder: str, download: bool):
-        reader = InHospitalMortalityReader(dataset_dir=mimic3_dir + f"/data/in-hospital-mortality/{folder}",
-                                                listfile=mimic3_dir + f"/data/in-hospital-mortality/{folder}/listfile.csv",
+    def prepare_data(self, mode: str,  folder: str, download: bool):
+        reader = InHospitalMortalityReader(dataset_dir=os.path.join(f"data/in-hospital-mortality/{folder}"),
+                                                listfile=os.path.join(f"data/in-hospital-mortality/{mode}_listfile.csv"),
                                                 period_length=48.0)
 
         (X, y, names) = read_and_extract_features(reader)
@@ -101,11 +100,10 @@ class MIMIC3(Dataset):
         
         # Save the data
         if download:
-            cached_dir = "datasets/cached"
-            if not os.path.exists(cached_dir):
-                os.makedirs(cached_dir)
-            X_cached = os.path.join(cached_dir, f'{folder}_X_cached.pt')
-            y_cached = os.path.join(cached_dir, f'{folder}_y_cached.pt')
+            if not os.path.exists(CACHED_DIR):
+                os.makedirs(CACHED_DIR)
+            X_cached = os.path.join(CACHED_DIR, f'{mode}_X_cached.pt')
+            y_cached = os.path.join(CACHED_DIR, f'{mode}_y_cached.pt')
             torch.save(X, X_cached)
             torch.save(y, y_cached)
 
